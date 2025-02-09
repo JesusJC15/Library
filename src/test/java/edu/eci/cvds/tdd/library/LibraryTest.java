3da4981943ea668c8d54ddf153df2895e9f67185
Natalia <natalia.espitia-e@mail.escuelaing.edu.co>
2025-02-09 01:29:56 -0500

    Se añadieron mas prueba del metodo loan

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -3,6 +3,7 @@ package edu.eci.cvds.tdd.library;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -62,6 +63,54 @@ public class LibraryTest {
         Loan loan = library.loanABook("1000097158", "9788467037769");
         assertNotNull(loan);
         assertEquals(LoanStatus.ACTIVE, loan.getStatus());
+    }
+
+    @Test
+    public void shouldNotLoanBookWithoutCopies() {
+        Book harryPotter1 = new Book("Harry Potter y la piedra filosofal", "JK Rowling", "9788497940933");
+        library.addBook(harryPotter1);
+        User natalia = new User();
+        natalia.setId("1000097158");
+        natalia.setName("Natalia");
+        library.addUser(natalia);
+        
+        library.loanABook("1000097158", "9788497940933");
+        Loan secondLoan = library.loanABook("1000097158", "9788497940933");
+        assertNull(secondLoan);
+    }
+
+    @Test
+    public void shouldNotLoanBookToUnregisteredUser() {
+        Book prideAndPrejuice = new Book("Orgullo y prejuicio", "Jane Austen", "9788497940933");
+        library.addBook(prideAndPrejuice);
+
+        Loan loan = library.loanABook("0000000000000", "9788497940933");
+        assertNull(loan);
+    }
+
+    @Test
+    public void shouldLoanMultipleCopiesToDifferentUsers() {
+        Book elPrincipito = new Book("El principito", "Antoine de Saint-Exupéry", "9788467037769");
+        User natalia = new User();
+        natalia.setId("1000097158");
+        natalia.setName("Natalia");
+
+        User jesus = new User();
+        jesus.setId("1000092885");
+        jesus.setName("Jesus");
+
+        library.addUser(natalia);
+        library.addUser(jesus);
+        library.addBook(elPrincipito);
+        library.addBook(elPrincipito); // Agregamos una segunda copia
+
+        Loan loan1 = library.loanABook("1000097158", "9788467037769");
+        Loan loan2 = library.loanABook("1000092885", "9788467037769");
+
+        assertNotNull(loan1);
+        assertNotNull(loan2);
+        assertEquals(LoanStatus.ACTIVE, loan1.getStatus());
+        assertEquals(LoanStatus.ACTIVE, loan2.getStatus());
     }
 
 }
